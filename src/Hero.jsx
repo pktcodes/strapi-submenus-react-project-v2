c95b2669c6a33d91a74dd4907fec08b57cd4c394
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-08-19 17:54:48 -0500

    Hero

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,5 +1,19 @@
 const Hero = () => {
-  return <h2>Hero</h2>;
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>
+          manage any content
+          <br />
+          anywhere
+        </h1>
+        <p>
+          Strapi is the leading open-source headless CMS. 100% JavaScript and
+          fully customizable.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
