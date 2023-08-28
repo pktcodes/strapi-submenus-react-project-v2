5bddd2cebdcf4e251eeedc8a642a3b4d74837682
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-08-28 01:02:57 -0500

    Hide Submenu (Part 2)

--- src/Submenu.jsx
+++ src/Submenu.jsx
@@ -1,14 +1,32 @@
 import { useGlobalContext } from './Context';
+import { useRef } from 'react';
 
 import links from './data';
 
 const Submenu = () => {
-  const { pageId } = useGlobalContext();
+  const { pageId, setPageId } = useGlobalContext();
 
   const currentPage = links.find((item) => item.pageId === pageId);
 
+  const submenuContainer = useRef(null);
+
+  const handleHideSubmenu = (event) => {
+    const submenu = submenuContainer.current;
+    const { left, right, bottom } = submenu.getBoundingClientRect();
+
+    const { clientX, clientY } = event;
+
+    if (clientX < left + 1 || clientX > right - 1 || clientY > bottom - 1) {
+      setPageId(null);
+    }
+  };
+
   return (
-    <div className={currentPage ? 'submenu show-submenu' : 'submenu'}>
+    <div
+      className={currentPage ? 'submenu show-submenu' : 'submenu'}
+      onMouseLeave={handleHideSubmenu}
+      ref={submenuContainer}
+    >
       <h5>{currentPage?.page}</h5>
       <div
         className="submenu-links"
